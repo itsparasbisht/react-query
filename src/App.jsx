f1c5c65f5740fe401aba6a94a81df95919e6181c
itsparasbisht <iamparasbisht@gmail.com>
2023-03-28 20:18:52 +0530

    useQuery hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  const { status, data, error } = useQuery({
+    queryKey: ["postsData"],
+    queryFn: () =>
+      axios("https://jsonplaceholder.typicode.com/posts").then(
+        (res) => res.data
+      ),
+  });
+
+  console.log(status);
+
+  if (status === "loading") {
+    return <span>Loading...</span>;
+  }
+
+  if (status === "error") {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <div>
+      {data.map((post) => (
+        <h2 key={post.id}>{post.title}</h2>
+      ))}
+    </div>
+  );
 }
 
 export default App;
